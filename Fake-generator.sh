1b84bd1c7679c6a6a243ed7265e6b5416f362dea
Thien Bui <thienbui797@gmail.com>
2018-03-19 11:19:16 +1100

    fake branch has to be gh-pages to get counted

--- Fake-generator.sh
+++ Fake-generator.sh
@@ -2,11 +2,11 @@ days=200
 total_commit=600
 size=$((days*24*60))
 a=$(shuf -i 1-$size -n $total_commit | sort -r -n)
-git checkout -b fake
+git checkout -b gh-pages
 for i in ${a}
 do 
 git commit --allow-empty -m "Fake commit"
 git commit --amend --allow-empty --no-edit --date "$(date -d "-$i minutes")"
 done 
-git push origin fake
+git push origin gh-pages
 git checkout master
