ec9e606f914addc655af992298587996c9c856e0
Thien Bui <thienbui797@gmail.com>
2018-03-19 11:17:40 +1100

    push to a fake branch instead of master

--- Fake-generator.sh
+++ Fake-generator.sh
@@ -2,12 +2,11 @@ days=200
 total_commit=600
 size=$((days*24*60))
 a=$(shuf -i 1-$size -n $total_commit | sort -r -n)
-
+git checkout -b fake
 for i in ${a}
 do 
 git commit --allow-empty -m "Fake commit"
 git commit --amend --allow-empty --no-edit --date "$(date -d "-$i minutes")"
 done 
-git add * 
-git commit -m "Inittial commit"
-git push -u origin master -f
+git push origin fake
+git checkout master
